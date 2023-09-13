772fa2559df3e052df2bc2fdb248324f3a52c2da
FilatovAE <filatovaleksander.job@gmail.com>
2023-09-13 19:36:47 +0300

    feat: add base layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { IconContext } from 'react-icons'
+import { CgMenu, CgClose } from 'react-icons/cg'
+import { MdTravelExplore, MdSettings } from 'react-icons/md'
+import { BiUserCircle } from 'react-icons/bi'
+
 import styles from './app.module.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+    <div>
+      <header className={styles.header}>
+        <div className='container'>
+          <div className={styles.header__inner}>
+            <button
+              className={styles.burgerBtn}
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <IconContext.Provider value={{ size: '40px' }}>
+                {menuOpen ? <CgClose /> : <CgMenu />}
+              </IconContext.Provider>
+            </button>
+            <h1>
+              Travel <span className={styles.logo__span}>App</span>
+            </h1>
+          </div>
+        </div>
+      </header>
+
+      <div className={menuOpen ? styles.menuOpen : styles.menuClose}>
+        <div className='container'>
+          <nav>
+            <ul className={styles.nav}>
+              <li>
+                <div> Мои путешествия</div>
+                <MdTravelExplore />
+              </li>
+              <li>
+                <div>Профиль</div> <BiUserCircle />
+              </li>
+              <li>
+                <div>Настройки</div> <MdSettings />
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <main>Home</main>
+    </div>
   )
 }
 
